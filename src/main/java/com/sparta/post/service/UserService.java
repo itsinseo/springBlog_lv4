2dd73244afd4c2555d904e78d2b17ee28775495d
Inseo Kim <itsinseo@gmail.com>
2023-06-26 20:55:30 +0900

    회원가입 기능 구현: User Entity 클래스에 생성자를 추가 UserRepository 에 findByUsername 메서드 선언 UserService 의 signup 메서드 작성

--- src/main/java/com/sparta/post/service/UserService.java
+++ src/main/java/com/sparta/post/service/UserService.java
@@ -4,14 +4,37 @@ import com.sparta.post.dto.LoginRequestDto;
 import com.sparta.post.dto.LoginResponseDto;
 import com.sparta.post.dto.SignupRequestDto;
 import com.sparta.post.dto.SignupResponseDto;
+import com.sparta.post.entity.User;
+import com.sparta.post.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 
     public SignupResponseDto signup(SignupRequestDto signupRequestDto) {
-        return null;
+        String username = signupRequestDto.getUsername();
+
+        // username 중복 확인
+        Optional<User> checkUsername = userRepository.findByUsername(username);
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자명입니다.");
+        }
+
+        String password = signupRequestDto.getPassword();
+        User user = new User(username, password);
+        userRepository.save(user);
+
+        return new SignupResponseDto("회원가입 성공", HttpStatus.OK.value());
     }
 
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
